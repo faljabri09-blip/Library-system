8875479856bbfcb71ff4d18ad1191056950eb0a2
Fatma <fatma@gmail.com>
2026-07-08 11:50:17 +0400

    Adding publisher model with constrain

---
+++ Library-system/Models/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library_system.Models
+{
+    public class Publisher
+    {
+        [Key] //primary key and not null
+        public string publisherCode { get; set; }
+
+        [Required]
+        public string publisherName { get; set; }
+
+        [Required]
+        public string publisherCountry { get; set; }
+    }
+}
--- Library-system/obj/Debug/net8.0/Library-system.AssemblyInfo.cs
+++ Library-system/obj/Debug/net8.0/Library-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25e9c5297475c7c0ba36e548c6cac881f98b566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b102f6ff7e3e93f6168c87d97edcc67c0b6e0fa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
