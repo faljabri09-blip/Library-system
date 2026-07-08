fc2cec999361747ebb27d43068b14248d78fa57a
Fatma <fatma@gmail.com>
2026-07-08 11:32:30 +0400

    Adding Vendor model

---
+++ Library-system/Vendor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_system
+{
+    public class Vendor
+    {
+        public string VendorCode { get; set; }
+
+        public int Contact_no { get; set; }
+    }
+}
