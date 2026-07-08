525022fb9841787029c82b963451e1c752a413df
Fatma <fatma@gmail.com>
2026-07-08 11:54:01 +0400

    Adding books model with constrain

---
+++ Library-system/Models/Books.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library_system.Models
+{
+    public class Books
+    {
+        [Key] //primary key and not null
+        public int bookId {  get; set; }
+
+        [Required]
+        public bool satus { get; set; }
+        [Required]
+        public decimal bookPrice { get; set; }
+    }
+}
--- Library-system/obj/Debug/net8.0/Library-system.AssemblyInfo.cs
+++ Library-system/obj/Debug/net8.0/Library-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b102f6ff7e3e93f6168c87d97edcc67c0b6e0fa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8875479856bbfcb71ff4d18ad1191056950eb0a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
