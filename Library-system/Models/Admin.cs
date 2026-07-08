b102f6ff7e3e93f6168c87d97edcc67c0b6e0fa8
Fatma <fatma@gmail.com>
2026-07-08 11:44:19 +0400

    Adding Admin model with constrain

---
+++ Library-system/Models/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library_system.Models
+{
+    public class Admin
+    {
+        [Key] // primary key and not null
+
+        public int AdminId { get; set; }
+
+        [Required] // not null
+        public string AdminName { get; set; }
+
+        [Required] //NotNull
+        public string contact_no { get; set; }
+
+    }
+}
--- Library-system/obj/Debug/net8.0/Library-system.AssemblyInfo.cs
+++ Library-system/obj/Debug/net8.0/Library-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc2cec999361747ebb27d43068b14248d78fa57a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a25e9c5297475c7c0ba36e548c6cac881f98b566")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
