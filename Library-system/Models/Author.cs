fe4cc3608d38bb88c28b39ec6fd139ac2532e061
Fatma <fatma@gmail.com>
2026-07-08 12:14:56 +0400

    Adding Author model with constrain

---
+++ Library-system/Models/Author.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library_system.Models
+{
+    public class Author
+    {
+
+        [Key]
+
+        public string authorCode { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string authorName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string authorSubject { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string authorQualification { get; set; }
+    }
+}
--- Library-system/obj/Debug/net8.0/Library-system.AssemblyInfo.cs
+++ Library-system/obj/Debug/net8.0/Library-system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b298a9785af8ca300a37a48f62efbb413e562951")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5610f30661a8adc36a9077b9fd3fa27139c79571")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library-system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
